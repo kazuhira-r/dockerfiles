daa89cc9523a107548d879a914ad0e7af92a3bdf
kazuhira <kazuhira.m@gmail.com>
2016-02-21 18:30:22 +0900

    add, Sense plugin, for Elasticsearch/Kibana.

--- kibana/start_kibana.sh
+++ kibana/start_kibana.sh
@@ -6,6 +6,9 @@ else
     ELASTICSEARCH_URL=http://localhost:9200
 fi
 
-perl -wpi -e 's!(.*)elasticsearch.url:.+!elasticsearch.url: "'${ELASTICSEARCH_URL}'"!' /opt/kibana/config/kibana.yml 
+perl -wpi -e 's!(.*)elasticsearch.url:.+!elasticsearch.url: "'${ELASTICSEARCH_URL}'"!' /opt/kibana/config/kibana.yml
+
+echo >> /opt/kibana/config/kibana.yml
+echo "sense.defaultServerUrl: ${ELASTICSEARCH_URL}" >> /opt/kibana/config/kibana.yml
 
 service kibana start && tailf /dev/null
