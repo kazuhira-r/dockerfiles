96be1385a7548ba7c523b399b379216faaeb2e2c
kazuhira <kazuhira.m@gmail.com>
2016-06-04 19:14:32 +0900

    add, easy user add script.

---
+++ rabbitmq/add-user.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+USERNAME=$1
+PASSWORD=$2
+
+sudo -u rabbitmq rabbitmqctl add_user ${USERNAME} ${PASSWORD}
+sudo -u rabbitmq rabbitmqctl set_permissions -p / ${USERNAME} ".*" ".*" ".*"
