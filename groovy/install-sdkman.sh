6290f82ceff0ade8fe0cc49d317224c5ae8c6461
kazuhira <kazuhira.m@gmail.com>
2016-11-03 17:25:01 +0900

    add, Groovy, Maven, Gradle, Spring Boot Dockerfile

---
+++ groovy/install-sdkman.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl -s "https://get.sdkman.io" | bash && \
+source "$HOME/.sdkman/bin/sdkman-init.sh" && \
+sdk install groovy && \
+sdk install gradle && \
+sdk install maven && \
+sdk install springboot
