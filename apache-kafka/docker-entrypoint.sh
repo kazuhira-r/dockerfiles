64ce0bcff109bf3132740937c9fea6dd09f09330
kazuhira <kazuhira.m@gmail.com>
2017-03-07 00:35:27 +0900

    enable, accept remote client request

--- apache-kafka/docker-entrypoint.sh
+++ apache-kafka/docker-entrypoint.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+SERVER_ADDRESS=`hostname -i`
+
+perl -wpi -e 's!^#listeners=PLAINTEXT://:9092!listeners=PLAINTEXT://'${SERVER_ADDRESS}':9092!' config/server.properties
+
 /opt/apache-kafka/bin/zookeeper-server-start.sh -daemon config/zookeeper.properties
 /opt/apache-kafka/bin/kafka-server-start.sh -daemon config/server.properties
 /opt/trifecta-ui/bin/trifecta-ui &
