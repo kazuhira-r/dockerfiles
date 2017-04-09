de07af445eb47ce43e8c7db8047a0b55492efea7
kazuhira <kazuhira.m@gmail.com>
2017-04-09 23:07:20 +0900

    support, management interface

--- infinispan-clustered/start-clustered-server.sh
+++ infinispan-clustered/start-clustered-server.sh
@@ -3,4 +3,5 @@
 LAUNCH_JBOSS_IN_BACKGROUND=1 \
     bin/standalone.sh -c clustered.xml \
     -Djboss.bind.address=`hostname -I` \
+    -Djboss.bind.address.management=`hostname -I` \
     "$@" >/dev/null
