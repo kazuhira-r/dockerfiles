14ad6ade78aaf1e9eda99087e2847280b7b77d89
kazuhira <kazuhira.m@gmail.com>
2017-04-29 22:01:20 +0900

    add, kafka-manager build script and Docker image include

---
+++ apache-kafka/build-kafka-manager.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+KAFKA_MANAGER_VERSION=1.3.3.6
+
+wget -q https://github.com/yahoo/kafka-manager/archive/${KAFKA_MANAGER_VERSION}.tar.gz
+tar -zxf ${KAFKA_MANAGER_VERSION}.tar.gz
+cd kafka-manager-${KAFKA_MANAGER_VERSION}
+
+./sbt clean dist
+
+mv target/universal/kafka-manager-${KAFKA_MANAGER_VERSION}.zip ../
+cd ..
+rm -rf ${KAFKA_MANAGER_VERSION}.tar.gz kafka-manager-${KAFKA_MANAGER_VERSION}
