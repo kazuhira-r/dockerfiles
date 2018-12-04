60ff36848f8e300e82c703ac1c0c63b64632d24f
kazuhira-r <kazuhira.m@gmail.com>
2018-12-04 23:51:14 +0900

    fix

--- add-nopass-sudo-user.sh
+++ add-nopass-sudo-user.sh
@@ -4,7 +4,7 @@ cat <<EOF
 adduser --disabled-login --gecos '' ${USERNAME} && \\
 usermod -G sudo ${USERNAME}
 
-echo "Defaults:${USERNAME} !requiretty" >> /etc/sudoers.d/${USERNAME} && \\
-echo "${USERNAME} ALL=(ALL)      NOPASSWD: ALL" >> /etc/sudoers.d/${USERNAME} && \\
+echo 'Defaults:${USERNAME} !requiretty' >> /etc/sudoers.d/${USERNAME} && \\
+echo '${USERNAME} ALL=(ALL)      NOPASSWD: ALL' >> /etc/sudoers.d/${USERNAME} && \\
 chmod 440 /etc/sudoers.d/${USERNAME}
 EOF
