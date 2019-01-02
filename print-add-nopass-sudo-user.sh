809985a9b9952492e2e6c48937a3d2d949ee781d
kazuhira-r <kazuhira.m@gmail.com>
2019-01-02 18:31:14 +0900

    fix, indent

--- print-add-nopass-sudo-user.sh
+++ print-add-nopass-sudo-user.sh
@@ -2,9 +2,9 @@ USERNAME=$1
 
 cat <<EOF
 RUN adduser --disabled-login --gecos '' ${USERNAME} && \\
-           usermod -G sudo ${USERNAME}
+    usermod -G sudo ${USERNAME}
 
 RUN echo "Defaults:${USERNAME} !requiretty" >> /etc/sudoers.d/${USERNAME} && \\
-           echo "${USERNAME} ALL=(ALL)      NOPASSWD: ALL" >> /etc/sudoers.d/${USERNAME} && \\
-           chmod 440 /etc/sudoers.d/${USERNAME}
+    echo "${USERNAME} ALL=(ALL)      NOPASSWD: ALL" >> /etc/sudoers.d/${USERNAME} && \\
+    chmod 440 /etc/sudoers.d/${USERNAME}
 EOF
