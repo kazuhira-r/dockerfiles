1f2a8c9d2b8d4cb0a317286f473940a038d97a68
kazuhira-r <kazuhira.m@gmail.com>
2019-01-03 01:44:29 +0900

    update, RabbitMQ 3.7.9-1

--- rabbitmq/add-user.sh
+++ rabbitmq/add-user.sh
@@ -5,3 +5,4 @@ PASSWORD=$2
 
 sudo -u rabbitmq rabbitmqctl add_user ${USERNAME} ${PASSWORD}
 sudo -u rabbitmq rabbitmqctl set_permissions -p / ${USERNAME} ".*" ".*" ".*"
+sudo -u rabbitmq rabbitmqctl set_user_tags ${USERNAME} management
