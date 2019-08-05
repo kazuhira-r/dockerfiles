7459a3ba75b55ea54cdb6ca2e9516f3093967dde
kazuhira-r <kazuhira.m@gmail.com>
2019-08-06 00:41:57 +0900

    add, insecure root user

--- mysql/docker-entrypoint.sh
+++ mysql/docker-entrypoint.sh
@@ -7,6 +7,19 @@ trap "kill $PID" INT
 /usr/sbin/mysqld &
 PID=$!
 
+sleep 5
+
 echo "'`mysql --version`' started."
+
+PASSWORD=password
+
+SQL_INSECURE_ROOT=$(cat <<EOS
+CREATE USER root@'%' IDENTIFIED BY '${PASSWORD}';
+GRANT ALL PRIVILEGES ON *.* TO root@'%' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+EOS
+)
+
+mysql -uroot -e "${SQL_INSECURE_ROOT}"
 
 tail -f /var/log/mysql/error.log
