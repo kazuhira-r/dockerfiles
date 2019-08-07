982600647b35aa01eb415167bc395a962aaef9f0
kazuhira-r <kazuhira.m@gmail.com>
2019-08-08 00:10:30 +0900

    prepare InnoDB Cluster

--- mysql/docker-entrypoint.sh
+++ mysql/docker-entrypoint.sh
@@ -4,10 +4,18 @@ PID=0
 
 trap "kill $PID" INT
 
+rm -f /var/lib/mysql/auto.cnf
+
+SERVER_ID=`hostname --ip-address | perl -wp -e 's!.+\.(\d+)!$1!'`
+perl -wpi -e 's!server_id = .+!server_id = '${SERVER_ID}'!' /etc/mysql/mysql.conf.d/mysqld.cnf
+
+SERVER_ADDRESS=`hostname -i`
+perl -wpi -e 's!report-host = ".+"!report-host = "'${SERVER_ADDRESS}'"!' /etc/mysql/mysql.conf.d/mysqld.cnf
+
 /usr/sbin/mysqld &
 PID=$!
 
-sleep 5
+sleep 10
 
 echo "'`mysql --version`' started."
 
@@ -17,6 +25,7 @@ SQL_INSECURE_ROOT=$(cat <<EOS
 CREATE USER root@'%' IDENTIFIED BY '${PASSWORD}';
 GRANT ALL PRIVILEGES ON *.* TO root@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
+RESET MASTER;
 EOS
 )
 
