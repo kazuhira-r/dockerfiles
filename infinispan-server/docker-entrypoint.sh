92d776a0dcdecc8b48d018abea7825560a53f503
kazuhira-r <kazuhira.m@gmail.com>
2019-09-28 19:03:36 +0900

    improve Infinispan Docker image

--- infinispan-server/docker-entrypoint.sh
+++ infinispan-server/docker-entrypoint.sh
@@ -3,8 +3,6 @@
 LAUNCH_ISPN_IN_BACKGROUND=1 \
     bin/server.sh \
     -Dinfinispan.bind.address=`hostname -i` \
-    "$@" >/dev/null 2>&1 &
+    "$@" &
 
-test -d server/log || mkdir server/log && \
-touch server/log/server.log && \
-tail -f server/log/server.log
+tail -f /dev/null
