0006d28160f774c06226adb532a4d3c4df7e26b7
kazuhira-r <kazuhira.m@gmail.com>
2019-09-28 18:36:47 +0900

    update, Infinispan 10.0.0.CR2

--- infinispan-server/docker-entrypoint.sh
+++ infinispan-server/docker-entrypoint.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-LAUNCH_JBOSS_IN_BACKGROUND=1 \
-    bin/standalone.sh \
-    -Djboss.bind.address=`hostname -i` \
-    -Djboss.bind.address.management=0.0.0.0 \
+LAUNCH_ISPN_IN_BACKGROUND=1 \
+    bin/server.sh \
+    -Dinfinispan.bind.address=`hostname -i` \
     "$@" >/dev/null 2>&1 &
 
-test -d standalone/log || mkdir standalone/log && \
-touch standalone/log/server.log && \
-tail -f standalone/log/server.log
+test -d server/log || mkdir server/log && \
+touch server/log/server.log && \
+tail -f server/log/server.log
