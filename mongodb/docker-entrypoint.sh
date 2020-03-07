26f5c84f5591859fdde86c0d3dfdde15d2a0db2a
kazuhira-r <kazuhira.m@gmail.com>
2020-03-08 01:07:08 +0900

    update, MongoDB 4.2.3

---
+++ mongodb/docker-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo perl -wpi -e 's!bindIp: 127.0.0.1!bindIp: 0.0.0.0!g' /etc/mongod.conf
+
+sudo /usr/bin/mongod --config /etc/mongod.conf &
+
+sleep 1
+
+sudo tail -f /var/log/mongodb/mongod.log
