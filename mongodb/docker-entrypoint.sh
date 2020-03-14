b2babcf5dfb744a4a67e945985ed0d6dd4a8dce8
kazuhira-r <kazuhira.m@gmail.com>
2020-03-14 20:29:43 +0900

    add, MongoDB startup time

--- mongodb/docker-entrypoint.sh
+++ mongodb/docker-entrypoint.sh
@@ -4,6 +4,6 @@ sudo perl -wpi -e 's!bindIp: 127.0.0.1!bindIp: 0.0.0.0!g' /etc/mongod.conf
 
 sudo /usr/bin/mongod --config /etc/mongod.conf &
 
-sleep 1
+sleep 3
 
 sudo tail -f /var/log/mongodb/mongod.log
