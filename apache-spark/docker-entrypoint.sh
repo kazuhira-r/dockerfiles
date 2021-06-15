2c8c92b6e2af39cf91a9cc4a47468918508d6fe9
kazuhira-r <kazuhira.m@gmail.com>
2021-06-16 00:35:06 +0900

    refactor

--- apache-spark/docker-entrypoint.sh
+++ apache-spark/docker-entrypoint.sh
@@ -2,14 +2,16 @@
 
 MODE=$1
 
+shift
+
 if [ "${MODE}" == 'master' ]; then
 
-    /opt/spark/sbin/start-master.sh
+    /opt/spark/sbin/start-master.sh "$@"
     tail -f /opt/spark/logs/spark-*.out
 
 elif [ "${MODE}" == "worker" ]; then
 
-    /opt/spark/sbin/start-worker.sh $2
+    /opt/spark/sbin/start-worker.sh "$@"
     tail -f /opt/spark/logs/spark--org.apache.spark.deploy.worker.Worker-*.out
 
 fi
