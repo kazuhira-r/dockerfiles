9dbced187ae9ef0d05ebd66cf033df70f1d1894e
kazuhira-r <kazuhira.m@gmail.com>
2021-06-26 02:02:08 +0900

    optimize, Infinispan Server image

--- infinispan-server/restart.sh
+++ infinispan-server/restart.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-touch /tmp/stdout.log
+SCRIPT_DIR=$(cd `dirname $0` && pwd)
+
+kill `pgrep server.sh`
+
+sleep 3
 
 LAUNCH_ISPN_IN_BACKGROUND=1 \
     bin/server.sh \
     -b 0.0.0.0 \
     -Djgroups.tcp.address=`hostname -i` \
-    "$@" > /tmp/stdout.log &
-
-tail -f /tmp/stdout.log
+    "$@" >> /tmp/stdout.log &
