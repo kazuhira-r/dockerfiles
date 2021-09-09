49136c63de973e801942629cca4231547f707935
kazuhira-r <kazuhira.m@gmail.com>
2021-09-09 22:06:26 +0900

    improve, CockroachDB image

--- cockroachdb/call-api.sh
+++ cockroachdb/call-api.sh
@@ -9,7 +9,7 @@ echo curl -H "'X-Cockroach-API-Session: $SESSION_TOKEN'" --cacert $CERTS_DIR/ca.
 
 echo
 
-echo curl -H "'X-Cockroach-API-Session: \$SESSION_TOKEN'" --cacert '$CERTS_DIR/ca.crt' https://localhost:8080$1
+echo curl -H '"X-Cockroach-API-Session: $SESSION_TOKEN"' --cacert '$CERTS_DIR/ca.crt' https://localhost:8080$1
 
 echo
 
