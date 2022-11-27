823ce418d8f875ae48dba60e4726be1f422c3bd8
kazuhira-r <kazuhira.m@gmail.com>
2022-11-28 00:21:37 +0900

    improve, Infinispan Server image

--- infinispan-server/docker-entrypoint.sh
+++ infinispan-server/docker-entrypoint.sh
@@ -5,7 +5,7 @@ touch /tmp/stdout.log
 LAUNCH_ISPN_IN_BACKGROUND=1 \
     bin/server.sh \
     -b 0.0.0.0 \
-    -Djgroups.tcp.address=`hostname -i` \
+    -Djgroups.tcp.address=$(hostname -i) \
     "$@" > /tmp/stdout.log &
 
 tail -f /tmp/stdout.log
