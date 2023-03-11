2ae5a78c6730aabbc556b7f9157a0038799d00b5
kazuhira-r <kazuhira.m@gmail.com>
2023-03-11 20:56:31 +0900

    update and refactor, RabbitMQ 3.11.0-1

--- rabbitmq/add-user.sh
+++ rabbitmq/add-user.sh
@@ -4,5 +4,5 @@ USERNAME=$1
 PASSWORD=$2
 
 sudo -u rabbitmq rabbitmqctl add_user ${USERNAME} ${PASSWORD}
-sudo -u rabbitmq rabbitmqctl set_permissions -p / ${USERNAME} ".*" ".*" ".*"
-sudo -u rabbitmq rabbitmqctl set_user_tags ${USERNAME} management
+sudo -u rabbitmq rabbitmqctl set_permissions -p / ${USERNAME} '.*' '.*' '.*'
+sudo -u rabbitmq rabbitmqctl set_user_tags ${USERNAME} monitoring
