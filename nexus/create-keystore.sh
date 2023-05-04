1bf778fbbe91a7be79ec6e1197ea76e8446e33ed
kazuhira-r <kazuhira.m@gmail.com>
2023-05-04 21:58:07 +0900

    update, Sonatype Nexus 3.53.0-01 and migration Eclipse Temurin

--- nexus/create-keystore.sh
+++ nexus/create-keystore.sh
@@ -2,4 +2,5 @@
 
 # CN => my.nexus.repo.example.com
 # password => password
-keytool -keystore etc/ssl/keystore.jks -alias nexus -genkey -keyalg RSA -sigalg SHA256withRSA
+#keytool -keystore etc/ssl/keystore.jks -alias nexus -genkey -keyalg RSA -sigalg SHA256withRSA
+keytool -keystore keystore.jks -alias nexus -genkey -keyalg RSA -sigalg SHA256withRSA
