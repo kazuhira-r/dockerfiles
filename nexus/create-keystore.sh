5af77a9d86c753e9a055a5525791c5e8ae105434
kazuhira-r <kazuhira.m@gmail.com>
2025-08-02 21:56:46 +0900

    update, Sonatype Nexus 3.82.0-08

--- nexus/create-keystore.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# CN => my.nexus.repo.example.com
-# password => password
-#keytool -keystore etc/ssl/keystore.jks -alias nexus -genkey -keyalg RSA -sigalg SHA256withRSA
-keytool -keystore keystore.jks -alias nexus -genkey -keyalg RSA -sigalg SHA256withRSA
